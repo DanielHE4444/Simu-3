2428acd8cf099d25d0f6dbb5cb8d7511572265d3
Daniel <danielhernandez.ee@academia.umb.edu.co>
2024-03-26 01:17:38 +0000

    Se hace el primer punto

---
+++ Punto1.m
@@ -0,0 +1,10 @@
+M=magic(3);
+disp(M);
+A = max(M,[],2);%Se calcula el maximo en cada fila
+disp (A);
+P = mean(M);%Se calcula el promedio
+disp(P);
+B = sort(M);%Se ordenan de orden acendente 
+disp (B);
+N=3*3;
+disp (N);%El numero de datos 
